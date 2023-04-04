cdfe99e152e461d7045754618158c8ed3093e556
Vedad Herceglija <Vedad.Herceglija@comtrade.com>
2023-04-04 18:36:48 +0200

    fluent validation

--- BuberDinner/BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/ApiController.cs
@@ -1,16 +1,30 @@
 using BuberDinner.Api.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 [ApiController]
 public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
-    { 
-        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
-        var firstError = errors.First();
+    {
+        if(errors == null || errors.Count == 0)
+        {
+            return Problem();
+        }
 
-        var statusCode = firstError.Type switch
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             ErrorType.Conflict => StatusCodes.Status409Conflict,
@@ -20,6 +34,19 @@ public class ApiController : ControllerBase
 
         return Problem(
             statusCode: statusCode,
-            title: firstError.Description);
+            title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+        return ValidationProblem(modelStateDictionary);
     }
 }
